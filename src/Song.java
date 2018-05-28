1202f19e17788dabae66ee25316714b6a263d107
Brandon Steel <bsteel364@gmail.com>
2018-05-28 19:20:13 -0400

    Inital Commit

---
+++ src/Song.java
@@ -0,0 +1,29 @@
+import java.io.File;
+
+public class Song {
+
+	private String name;
+	private String artist;
+	private File tab;
+	
+	public Song(String songName, String songArtist, File songTab) {
+		this.name = name;
+		this.artist = songArtist;
+		this.tab = songTab;
+	}
+	
+	public Song(String songName, String songArtist) {
+		this.name = name;
+		this.artist = songArtist;
+	}
+	
+	public Song(String songName, File songTab) {
+		this.name = name;
+		this.tab = songTab;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+}
